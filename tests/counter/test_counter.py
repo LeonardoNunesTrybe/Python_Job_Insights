65ebf3235d9426099a0be34d383f82f0a8c7d01b
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-11-26 19:08:33 +0000

    feat: requisito 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    path = 'data/jobs.csv'
+    word_to_count_1 = 'Python'
+    word_to_count_2 = 'Javascript'
+    assert count_ocurrences(path, word_to_count_1) == 1
+    assert count_ocurrences(path, word_to_count_2) == 1
