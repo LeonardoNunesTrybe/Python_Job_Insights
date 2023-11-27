13400ca2309a466bc846dff8cca250fa44bd05df
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-11-27 00:08:48 +0000

    feat: requisito 10

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    path = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert isinstance(path, list)
+    assert all(isinstance(item, dict) for item in path)
+
+    keys = path[0].keys() if path else []
+    expected_keys = {"title", "salary", "type"}
+
+    assert all(key in expected_keys for key in keys)
